76e107c4ad1fd7a4bd13cf5f1625d67b51f4668c
Евгений Бучнев <noname95689@yandex.ru>
2024-03-08 17:45:14 +0300

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] intArray= {1, 7, 11, -1, -5};
+        int first = intArray[0];
+        int last = intArray[intArray.length -1];
+        intArray[0] = last;
+        intArray[intArray.length - 1] = first;
+        System.out.println(intArray[0] + (intArray.length / 2));
     }
 }
