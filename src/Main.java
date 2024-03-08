4ee44b64fdb8fa3e2a50446538e2d3b7e9c9c361
Евгений Бучнев <noname95689@yandex.ru>
2024-03-08 18:04:44 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите число x: ");
+        int x = console.nextInt();
+        System.out.println("Введите число y: ");
+        int y = console.nextInt();
+        System.out.println("Введите число z: ");
+        int z = console.nextInt();
+        int mathAverage = (x + y + z)/3;
+        System.out.println("Среднее арифметическое введенных чисел: " + mathAverage);
+        // Для целых чисел остаток от деления и так игноируется, поэтому не округляю результат.
+        if ((mathAverage/2) > 3) {
+            System.out.println("Программа выполнена корректно");
+        }
     }
 }
