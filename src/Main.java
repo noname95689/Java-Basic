55a75b0e452303f05cb9568ec415498e394c93c8
Евгений Бучнев <noname95689@yandex.ru>
2024-03-08 21:43:51 +0300

    Задача 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        int a = Integer.parseInt(console.nextLine());
+        System.out.println("Введите второе число: ");
+        int b = Integer.parseInt(console.nextLine());
+        System.out.println("Введите символ математической операции");
+        String operation = console.nextLine();
+        System.out.println(operation);
+        int result = switch (operation) {
+            case "+" -> a + b;
+            case "-" -> a - b;
+            case "*" -> a * b;
+            case "/" -> a / b;
+            default -> 0;
+        };
+        System.out.println("Результат операции: " + result);
     }
 }
