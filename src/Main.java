dedc470f2bd2ff8953e63235c2ad18a877263255
Евгений Бучнев <noname95689@yandex.ru>
2024-03-10 19:38:54 +0300

    Задача 10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] multiArray = createDoubleDimensionalArray();
+        for (int i = 0; i < multiArray[0].length; i++) {
+            System.out.println(multiArray[0][i]*3);
+        }
+    }
+
+    public static int[][] createDoubleDimensionalArray() {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите количество строк массива: ");
+        int n = console.nextInt();
+        System.out.println("Введите количество столбцов массива: ");
+        int m = console.nextInt();
+        int[][] doubleDimensionalArray = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                doubleDimensionalArray[i][j] = console.nextInt();
+            }
+        }
+        return doubleDimensionalArray;
     }
 }
