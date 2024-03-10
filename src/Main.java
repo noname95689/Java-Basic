7d12c8c2056f97c666efe595d8f043469a2bd4bc
Евгений Бучнев <noname95689@yandex.ru>
2024-03-10 17:50:26 +0300

    Задача 9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double[] userArray = createArray();
+        double mathAverage = getMathAverage(userArray);
+        System.out.println("Среднее арифметическое: " + mathAverage);
+        for (int i = 0; i < userArray.length; i++) {
+            System.out.println(userArray[i]*mathAverage);
+        }
     }
+
+    public static double[] createArray() {
+        System.out.println("Введите размер массива: ");
+        Scanner console = new Scanner(System.in);
+        int arraySize = console.nextInt();
+        double[] userArray = new double[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            userArray[i] = console.nextDouble();
+        }
+        return userArray;
+    }
+
+    public static double getMathAverage(double[] userArray) {
+        double sum = 0;
+        for (int i = 0; i < userArray.length; i++) {
+            sum = sum + userArray[i];
+        }
+        return sum/ userArray.length;
+    }
+
 }
