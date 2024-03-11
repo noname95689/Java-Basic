17d242e9dc37f4c3f7b3b86057d64883278b74d4
Евгений Бучнев <noname95689@yandex.ru>
2024-03-11 21:24:37 +0300

    Задача 11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        String numericString;
+        if (console.hasNextInt()) {
+            numericString = String.valueOf(console.nextInt());
+        } else {
+            System.out.println("Пожалуйста, введите целое число");
+            return;
+        }
+        System.out.println();
+        int intNumber;
+        if (console.hasNextInt()) {
+            intNumber = console.nextInt();
+        } else {
+            System.out.println("Пожалуйста, введите целое число");
+            return;
+        }
+        if ((Integer.valueOf(numericString) >= intNumber)) {
+            System.out.println("Наибольшее число: " + numericString);
+            System.out.println("Наименьшее число преобразованное в double: " + Double.valueOf(intNumber));
+        } else {
+            System.out.println("Наибольшее число: " + intNumber);
+            System.out.println("Наименьшее число преобразованное в double: " + Double.valueOf(numericString));
+        }
     }
 }
