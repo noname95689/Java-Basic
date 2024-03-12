89e285eb0bb3305a0bd3d246e463d329df535889
Евгений Бучнев <noname95689@yandex.ru>
2024-03-12 22:03:43 +0300

    Задача 14

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,20 @@ public class Main {
             intArray[i] =  random.nextInt(41) - 20;
         }
         System.out.println("Выводим минимальное число из массива");
-        System.out.println(getMinValue(intArray));
+        int min = getMinValue(intArray);
+        System.out.println(min);
         System.out.println("Выводим максимальное число из массива");
-        System.out.println(getMaxValue(intArray));
+        int max = getMaxValue(intArray);
+        System.out.println(max);
+        System.out.println("Выводим максимальное по модулю число");
+        System.out.println(getMaxAbsValue(min, max));
     }
 
     public static int getMinValue(int[] intArray) {
         int MIN_VALUE = Integer.MAX_VALUE;
-        for (int i = 0; i < intArray.length; i++) {
-            if (intArray[i] <= MIN_VALUE) {
-                MIN_VALUE = intArray[i];
+        for (int j : intArray) {
+            if (j <= MIN_VALUE) {
+                MIN_VALUE = j;
             }
         }
         return MIN_VALUE;
@@ -25,11 +29,15 @@ public class Main {
 
     public static int getMaxValue(int[] intArray) {
         int MAX_VALUE = Integer.MIN_VALUE;
-        for (int i = 0; i < intArray.length; i++) {
-            if (intArray[i] >= MAX_VALUE) {
-                MAX_VALUE = intArray[i];
+        for (int j : intArray) {
+            if (j >= MAX_VALUE) {
+                MAX_VALUE = j;
             }
         }
         return MAX_VALUE;
+    }
+
+    public static int getMaxAbsValue(int a, int b) {
+        return (Math.abs(a) >= Math.abs(b) ? a : b);
     }
 }
