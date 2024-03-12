2e6f9c980e8055cedf70b6eb8a51f8a0d03a3af7
Евгений Бучнев <noname95689@yandex.ru>
2024-03-12 21:05:37 +0300

    Задача 13

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите строку из слов разделенных пробелами: ");
+        String input = console.nextLine();
+        String[] inputArray = input.split(" ");
+        int latinWordsCount = 0;
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i].matches("[a-zA-Z]+")) {
+                System.out.println(inputArray[i]);
+                latinWordsCount++;
+            }
+        }
+        System.out.println("Количество слов состоящих из латиницы: " + latinWordsCount);
     }
 }
